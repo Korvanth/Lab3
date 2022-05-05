648abab7ed8ebdfcb6b3b4afa79de41957e2a52c
Matthew Joshua Ou Yong <MJOY.21@ichat.sp.edu.sg>
2022-05-05 11:29:40 +0800

    Lab 3 Ex 1

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -5,8 +5,8 @@ print("Test_Lab3")
 
 def test_bubble_sort_ascending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [11, 12, 22, 25, 34, 64, 90]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 100, 122, 21]
+    test_arr = [11, 12, 21, 22, 25, 34, 64, 90, 100, 122]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
 
@@ -14,8 +14,8 @@ def test_bubble_sort_ascending():
 
 def test_bubble_sort_descending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [90, 64, 34, 25, 22, 12, 11]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 100, 122, 21]
+    test_arr = [122, 100, 90, 64, 34, 25, 22, 21, 12, 11]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
@@ -23,8 +23,11 @@ def test_bubble_sort_descending():
 
 def test_bubble_sort_invalid():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 100, 122, 21]
 
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+
+
